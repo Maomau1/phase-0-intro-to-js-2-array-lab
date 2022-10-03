5e747f98a2abaa719013dac67b78a7a819e8c3a7
maurice diong <mdiong00@gmail.com>
2022-10-02 22:09:17 -0400

    commit tree 63d449974c8135791a6ae5d3c28b845c3efcb68a

--- index.js
+++ index.js
@@ -1,1 +1,123 @@
 // Write your solution here!
+/* describe('index.js', function () {
+  describe('cats', function () {
+    it('is assigned an initial value of ["Milo", "Otis", "Garfield"]', function () {
+      expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+    });
+  });
+
+  describe('Array functions', function () {
+    beforeEach(function () {
+      cats.length = 0;
+
+      cats.push('Milo', 'Otis', 'Garfield');
+    });
+    
+    describe('destructivelyAppendCat(name)', function () {
+      it('appends a cat to the end of the cats array', function () {
+        destructivelyAppendCat('Ralph');
+
+        expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield", "Ralph"]);
+      });
+    });
+
+    describe('destructivelyPrependCat(name)', function () {
+      it('prepends a cat to the beginning of the cats array', function () {
+        destructivelyPrependCat("Bob");
+
+        expect(cats).to.have.ordered.members(["Bob", "Milo", "Otis", "Garfield"]);
+      });
+    });
+    
+    
+    describe('destructivelyRemoveLastCat()', function () {
+      it('removes the last cat from the cats array', function () {
+        destructivelyRemoveLastCat();
+
+        expect(cats).to.have.ordered.members(["Milo", "Otis"]).and.to.not.include('Garfield');
+      });
+    });
+
+    describe('destructivelyRemoveFirstCat()', function () {
+      it('removes the first cat from the cats array', function () {
+        destructivelyRemoveFirstCat();
+        
+        
+        expect(cats).to.have.ordered.members(["Otis", "Garfield"]).and.to.not.include('Milo');
+      });
+    });
+
+    describe('appendCat(name)', function () {
+      it('appends a cat to the cats array and returns a new array, leaving the cats array unchanged', function () {
+        expect(appendCat("Broom")).to.have.ordered.members(["Milo", "Otis", "Garfield", "Broom"]);
+
+        expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+      });
+    });
+
+
+     describe('prependCat(name)', function () {
+      it('prepends a cat to the cats array and returns a new array, leaving the cats array unchanged', function () {
+        expect(prependCat("Arnold")).to.have.ordered.members(["Arnold", "Milo", "Otis", "Garfield"]);
+
+        expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+      });
+    });
+
+    describe('removeLastCat()', function () {
+      it('removes the last cat in the cats array and returns a new array, leaving the cats array unchanged', function () {
+        expect(removeLastCat()).to.have.ordered.members(["Milo", "Otis"]);
+
+        expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+      });
+    });
+
+    describe('removeFirstCat()', function () {
+      it('removes the first cat from the cats array and returns a new array, leaving the cats array unchanged', function () {
+        expect(removeFirstCat()).to.have.ordered.members(["Otis", "Garfield"]);
+
+        expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+      });
+*/
+
+    const cats = ["Milo", "Otis", "Garfield"];
+
+    const destructivelyAppendCat = function (name) {
+        return cats.push(name);
+    }
+
+    const destructivelyPrependCat = function (name) {
+        return cats.unshift(name);
+    }
+
+    const destructivelyRemoveLastCat = function () {
+        return cats.pop();
+    }
+
+    const destructivelyRemoveFirstCat = function () {
+        return cats.shift();
+    }
+
+    const appendCat = function(name) {
+        return [...cats, name];
+    }
+
+    const prependCat = function (name) {
+        return [name, ...cats];
+    }
+
+    const removeFirstCat = function() {
+       const newCats = [...cats];
+        newCats.shift();
+        return newCats;
+    }
+
+    const removeLastCat = function() {
+       const newCats = cats.slice()
+        newCats.pop();
+        return newCats;
+
+    }
+
+
+
